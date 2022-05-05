0e3f32a1724f7ea57aacf68ea7ef69f1bbb8f3bb
Bryant Lee <cloud9bryant@gmail.com>
2022-05-05 16:18:26 -0700

    Added Gabriel's logout code from dev

--- login.js
+++ login.js
@@ -16,6 +16,24 @@ app.use(session({
     saveUninitialized: true
 }));
 
+// Gabriel's Logout code
+const express = require("express");
+const session = require("express-session");
+const app = express();
+app.get("/logout", function(req,res){
+
+    if (req.session) {
+        req.session.destroy(function(error) {
+            if (error) {
+                res.status(400).send("Unable to log out") //Can Change This Message If We Want
+            } else {
+                // session deleted, redirect to home
+                res.redirect("/");
+            }
+        });
+    }
+});
+
 app.get("/", function (req, res) {
     if (req.session.loggedIn) {
         if (req.session.admin) {
