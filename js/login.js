6dbb59047742c7ae0aa08581b5f9cafe4ed3ca4d
Bryant Lee <cloud9bryant@gmail.com>
2022-05-06 11:35:47 -0700

    Updated pathways for login.js

--- js/login.js
+++ js/login.js
@@ -30,7 +30,7 @@ app.get("/", function (req, res) {
             console.log("Logged In.");
         }
     } else {
-        let doc = fs.readFileSync("./index.html", "utf8");
+        let doc = fs.readFileSync("../index.html", "utf8");
         res.set("Server", "TechToTheMoon Engine");
         res.set("X-Powered-By", "MoonPC");
         res.send(doc);
@@ -41,7 +41,7 @@ app.get("/profile/:user_name", function (req, res) {
     if (req.session.loggedIn && req.session.user_name === req.params.user_name) {
         // TODO: create profile html page
         // and then replace main with profile
-        let doc = fs.readFileSync("./template.html", "utf8");
+        let doc = fs.readFileSync("../template.html", "utf8");
         res.send(doc);
     } else {
         res.redirect('/');
@@ -52,7 +52,7 @@ app.get("/admin/:user_name", function (req, res) {
     if (req.session.loggedIn && req.session.admin && req.session.user_name === req.params.user_name) {
         // TODO: create admin html page
         // and then replace main with admin
-        let doc = fs.readFileSync("./admin.html", "utf8");
+        let doc = fs.readFileSync("../admin.html", "utf8");
         res.send(doc);
     } else {
         res.redirect('/');
