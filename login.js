e92e028a7130142718a7bdf502ff67258e274e82
Bryant Lee <cloud9bryant@gmail.com>
2022-05-05 23:33:36 -0700

    Working login and logout functions, added get request for pages

--- login.js
+++ login.js
@@ -10,26 +10,11 @@ app.use("/css", express.static("./app/css"));
 app.use("/img", express.static("./app/img"));
 
 app.use(session({
-    secret: "caleMakarIsCool",
+    secret: "CaleMakarIsCool",
     name: "2800BBY29",
     resave: false,
     saveUninitialized: true
 }));
-
-// Gabriel's Logout code
-app.get("/logout", function(req,res){
-
-    if (req.session) {
-        req.session.destroy(function(error) {
-            if (error) {
-                res.status(400).send("Unable to log out") //Can Change This Message If We Want
-            } else {
-                // session deleted, redirect to home
-                res.redirect("/");
-            }
-        });
-    }
-});
 
 app.get("/", function (req, res) {
     if (req.session.loggedIn) {
@@ -38,8 +23,8 @@ app.get("/", function (req, res) {
             res.redirect(`/admin/${req.session.user_name}`);
             console.log("Logged In.");
         } else {
-        res.redirect(`/profile/${req.session.user_name}`);
-        console.log("Logged In.");
+            res.redirect(`/profile/${req.session.user_name}`);
+            console.log("Logged In.");
         }
     } else {
         let doc = fs.readFileSync("./app/html/main.html", "utf8");
@@ -50,30 +35,24 @@ app.get("/", function (req, res) {
 });
 
 app.get("/profile/:user_name", function (req, res) {
-    if (req.session.loggedIn) {
-        if (req.session.user_name === req.params.user_name) {
-            let doc = fs.readFileSync("./app/html/profile.html", "utf8");
-            res.send(doc);
-        } else {
-            res.redirect(`/profile/${req.session.user_name}`);
-        }
+    if (req.session.loggedIn && req.session.user_name === req.params.user_name) {
+        // TODO: create profile html page
+        // and then replace main with profile
+        let doc = fs.readFileSync("./app/html/main.html", "utf8");
+        res.send(doc);
     } else {
-        res.redirect("/");
+        res.redirect('/');
     }
 });
 
 app.get("/admin/:user_name", function (req, res) {
-    if (req.session.loggedIn && req.session.admin) {
-        if (req.session.user_name === req.params.user_name) {
-            // TODO: create admin html page
-            let doc = fs.readFileSync("./app/html/admin.html", "utf8");
-            res.send(doc);
-        } else {
-            // TODO: CHANGE USER ID TO DISPLAY USERNAME
-            res.redirect(`/admin/${req.session.user_name}`);
-        }
+    if (req.session.loggedIn && req.session.admin && req.session.user_name === req.params.user_name) {
+        // TODO: create admin html page
+        // and then replace main with admin
+        let doc = fs.readFileSync("./app/html/main.html", "utf8");
+        res.send(doc);
     } else {
-        res.redirect("/");
+        res.redirect('/');
     }
 });
 
@@ -85,13 +64,11 @@ app.post("/login", function (req, res) {
         host: '127.0.0.1',
         port: 3306,
         user: 'root',
-        password: 'comp2800',
-        database: 'project'
+        password: 'comp1537',
+        database: 'techtothemoon'
     });
     connection.connect(function (err) {
         if (err) {
-            console.log('test');
-            res.send(err);
             return console.error('error: ' + err);
         }
     });
@@ -114,8 +91,12 @@ app.post("/login", function (req, res) {
                     req.session.save(function (err) {
                         console.log("Session saved.");
                     });
-                    console.log(results)
-                    res.send(results);
+                    const resObj = {
+                        user_name: results[0].user_name,
+                        first_name: results[0].first_name,
+                        admin: results[0].admin
+                    };
+                    res.send(resObj);
                 } else {
                     res.sendStatus(401);
                     console.log('test');
@@ -126,14 +107,13 @@ app.post("/login", function (req, res) {
     );
 });
 
-app.get("/user_timeline/:user_name", function (req, res) {
-    const user_ID = req.params.user_name;
+app.get("/users", function (req, res) {
     const connection = mysql.createConnection({
         host: '127.0.0.1',
         port: 3306,
         user: 'root',
-        password: 'comp2800',
-        database: 'project'
+        password: 'comp1537',
+        database: 'techtothemoon'
     });
     connection.connect(function (err) {
         if (err) {
@@ -142,8 +122,7 @@ app.get("/user_timeline/:user_name", function (req, res) {
         }
     });
     connection.execute(
-        "SELECT * FROM 2800_202210_BBY29_user_timeline WHERE 2800_202210_BBY29_user_timeline.user_ID = ?",
-        [user_ID],
+        "SELECT * FROM 2800_202210_BBY29_user",
         function (error, results, fields) {
             if (error) {
                 console.log(error);
@@ -160,18 +139,56 @@ app.get("/user_timeline/:user_name", function (req, res) {
     );
 });
 
-// app.get("/logout", function (req, res) {
-//     if (req.session) {
-//         req.session.destroy(function (error) {
-//             if (error) {
-//                 res.status(400).send("Server error.")
-//             } else {
-//                 console.log("Session destroyed.")
-//                 res.sendStatus(200);
-//             }
-//         });
-//     }
-// });
+// Gabriel's Logout code
+app.post("/logout", function (req, res) {
+
+    if (req.session) {
+        req.session.destroy(function (error) {
+            if (error) {
+                res.status(400).send("Unable to log out") //Can Change This Message If We Want
+            } else {
+                res.sendStatus(200);
+            }
+        });
+    }
+});
+
+app.post('/add-user', function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+
+    console.log("userName", req.body.user_name);
+    console.log("firstName", req.body.first_name);
+    console.log("lastName", req.body.last_name);
+    console.log("Email", req.body.email);
+    console.log("phoneNumber", req.body.phone_number);
+    console.log("Password", req.body.password);
+
+    let connection = mysql.createConnection({
+        host: '127.0.0.1',
+        port: 3306,
+        user: 'root',
+        password: 'comp1537',
+        database: 'techtothemoon'
+    });
+    connection.connect();
+    // TO PREVENT SQL INJECTION, DO THIS:
+    // (FROM https://www.npmjs.com/package/mysql#escaping-query-values)
+    connection.query('INSERT INTO 2800_202210_BBY29_user (user_name, first_name, last_name, email, phone_number, password) values (?, ?, ?, ?, ?, ?)',
+        [req.body.user_name, req.body.first_name, req.body.last_name, req.body.email, req.body.phone_number, req.body.password],
+        function (error, results, fields) {
+            if (error) {
+                console.log(error);
+            }
+            //console.log('Rows returned are: ', results);
+            res.send({
+                status: "success",
+                msg: "Record added."
+            });
+
+        });
+    connection.end();
+
+});
 
 let port = 8000;
 app.listen(port, function () {
