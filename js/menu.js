33f16053a5009dc6d7c08655f877d77fb8ce246d
Jacob Romano Carlsen <jromanocarlsen@gmail.com>
2022-05-10 16:50:31 -0700

    Removed some of the elements that would be dynamically added to profile menu for privacy reasons

--- js/menu.js
+++ js/menu.js
@@ -10,27 +10,17 @@ function getCurrentUser() {
 
       const userNameLocation = document.getElementById("placeUsernameHere");
       const fullNameLocation = document.getElementById("placeFullNameHere");
-      const emailLocation = document.getElementById("placeEmailHere");
-      const phoneNumberLocation = document.getElementById("placePhoneNumHere");
 
       var usernameEl = document.createElement("p");
       var fullNameEl = document.createElement("p");
-      var emailEl = document.createElement("p");
-      var pNumEl = document.createElement("p");
       var text1 = document.createTextNode(response[0].user_name);
       var text2 = document.createTextNode(
         response[0].first_name + " " + response[0].last_name
       );
-      var text3 = document.createTextNode(response[0].email);
-      var text4 = document.createTextNode(response[0].phone_number);
       usernameEl.appendChild(text1);
       fullNameEl.appendChild(text2);
-      emailEl.appendChild(text3);
-      pNumEl.appendChild(text4);
       userNameLocation.appendChild(usernameEl);
       fullNameLocation.appendChild(fullNameEl);
-      emailLocation.appendChild(emailEl);
-      phoneNumberLocation.appendChild(pNumEl);
     }
   };
 
