c8b8869ad673989e3fffbaefeed37e39f753c246
Jacob Romano Carlsen <jromanocarlsen@gmail.com>
2022-05-11 14:07:36 -0700

    Created the function that would handle sending the information from the form to the database and verifying its success/failure with an alert to the user

--- js/account.js
+++ js/account.js
@@ -3,6 +3,9 @@ document
   .getElementById("verifyButton")
   .addEventListener("click", verifyPassword);
 document.getElementById("cancelForm").addEventListener("click", getCurrentUser);
+document
+  .getElementById("confirmEditButton")
+  .addEventListener("click", submitChanges);
 
 function getCurrentUser() {
   var xhr = new XMLHttpRequest();
@@ -43,17 +46,36 @@ function hideModal() {
   instance.hide();
 }
 
+function hideConfirmModal() {
+  var modal = document.getElementById("confirmModal");
+  var instance = bootstrap.Modal.getInstance(modal);
+  instance.hide();
+}
+
 function showSaveButton() {
   const form = document.getElementById("editAccountForm");
   const button = document.createElement("button");
   button.setAttribute("type", "button");
   button.setAttribute("class", "btn btn-primary float-end formOptions");
   button.setAttribute("id", "confirmChangeButton");
-  button.setAttribute("data-bs-toggle", "modal");
-  button.setAttribute("data-bs-target", "#exampleModal");
+  button.setAttribute("onclick", "verifyForm()");
+
   var text = document.createTextNode("Save");
   button.appendChild(text);
   form.appendChild(button);
+}
+
+function verifyForm() {
+  //TODO: Fix form validation method
+  const pass1 = document.getElementById("newPasswordInput").value;
+  const pass2 = document.getElementById("confirmPasswordInput").value;
+
+  if (pass1 === pass2) {
+    var confirmModal = new bootstrap.Modal(
+      document.getElementById("confirmModal")
+    );
+    confirmModal.show();
+  }
 }
 
 async function verifyPassword() {
@@ -93,5 +115,61 @@ async function verifyPassword() {
     }
   } catch (error) {
     console.log(error);
+  }
+}
+
+async function submitChanges() {
+  const data = {
+    user_name: document.getElementById("usernameInput").value,
+    first_name: document.getElementById("fNameInput").value,
+    last_name: document.getElementById("lNameInput").value,
+    email: document.getElementById("emailInput").value,
+    phone_number: document.getElementById("phoneNumInput").value,
+    password: document.getElementById("newPasswordInput").value,
+  };
+  try {
+    let responseObject = await fetch("/updateData", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    if (responseObject.status === 200) {
+      //Produce alert message telling user it worked
+      var alertLocation = document.getElementById("alertLocation");
+      var tempPass = document.getElementById("alert-pass");
+      var container = document.createElement("div");
+      if (!tempPass) {
+        container.innerHTML =
+          '<div id="alert-pass" class="alert alert-' +
+          "success" +
+          ' role="alert">' +
+          "Your account has successfully been updated." +
+          "</div>";
+        alertLocation.append(container);
+      }
+      getCurrentUser();
+      hideConfirmModal();
+    } else {
+      var alertPlaceholder = document.getElementById("alertLocation");
+      var temp = document.getElementById("alert-created");
+      var wrapper = document.createElement("div");
+      if (!temp) {
+        wrapper.innerHTML =
+          '<div id="alert-created" class="alert alert-' +
+          "danger" +
+          ' role="alert">' +
+          "An error occurred while updating your account." +
+          "</div>";
+        alertPlaceholder.append(wrapper);
+      }
+      hideConfirmModal();
+      console.log(responseObject.status);
+    }
+  } catch (error) {
+    console.log(error);
+    console.log("There was a problem with updating your account.");
   }
 }
