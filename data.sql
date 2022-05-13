bda75ea9addb7fda16a8a60df5be26de36a30ee6
Jacob Romano Carlsen <jromanocarlsen@gmail.com>
2022-05-13 12:53:21 -0700

    Added a column into bby29_user that would allow for user's avatar to be customized

--- data.sql
+++ data.sql
@@ -9,12 +9,13 @@ CREATE TABLE BBY29_user (
   phone_number VARCHAR(20),
   admin boolean,
   password VARCHAR(50),
+  avatar_path int,
   PRIMARY KEY (ID)
 );
 
-INSERT INTO BBY29_user (user_name, first_name, last_name, email, phone_number, admin, password) VALUES
-('bby29test', 'BBY', 'Test', 'insertemailhere@bcit.ca', '1234567890', 0, 'testtest'),
-('BryantSWE', 'Bryant', 'Lee', 'bryant@bcit.ca', '6048224011', 1, 'bryanttest'),
-('KasraE', 'Kasra', 'Esfa', 'kasra@bcit.ca', '6046046046', 1, 'Kasra'),
-('jacobrc10', 'Jacob', 'Romano Carlsen', 'jacob@bcit.ca', '6041234567', 1, 'adminpassword'),
-('gabrielC', 'Gabriel', 'Clarin',  'gclarin@my.bcit.ca', '6044688425', 1,  'gabrieltest');
+INSERT INTO BBY29_user (user_name, first_name, last_name, email, phone_number, admin, password, avatar_path) VALUES
+('bby29test', 'BBY', 'Test', 'insertemailhere@bcit.ca', '1234567890', 0, 'testtest', 0),
+('BryantSWE', 'Bryant', 'Lee', 'bryant@bcit.ca', '6048224011', 1, 'bryanttest', 0),
+('KasraE', 'Kasra', 'Esfa', 'kasra@bcit.ca', '6046046046', 1, 'Kasra', 0),
+('jacobrc10', 'Jacob', 'Romano Carlsen', 'jacob@bcit.ca', '6041234567', 1, 'adminpassword', 0),
+('gabrielC', 'Gabriel', 'Clarin',  'gclarin@my.bcit.ca', '6044688425', 1,  'gabrieltest', 0);
