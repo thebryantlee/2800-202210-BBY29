58dbd4cf737b14393f779c6c2795b233cc6e2bee
Jacob Romano Carlsen <jromanocarlsen@gmail.com>
2022-05-19 09:38:47 -0700

    Added the frontend code for news features and the modal required for it

--- js/home.js
+++ js/home.js
@@ -7,7 +7,7 @@ function getRecentNews() {
     '<div class="col-md-6 px-0">' +
     '<h1 class="display-4 fst-italic" id="primaryRecentNewsTitle">Title of a longer featured blog post</h1>' +
     '<p class="lead my-3" id="primaryRecentNewsSample">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.</p>' +
-    '<p class="lead mb-0"><a href="#" class="text-primary fw-bold" id="primaryRecentNewsLink">Continue reading &#8811;</a></p>' +
+    '<p class="lead mb-0"><button class="btn btn-outline-primary fw-bold" id="primaryRecentNewsLink">Continue reading &#8811;</button></p>' +
     "</div>" +
     "</div>";
   newsLocation.innerHTML +=
@@ -19,7 +19,7 @@ function getRecentNews() {
     '<h3 class="mb-0" id="secondaryNews1Title">Featured post</h3>' +
     '<div class="mb-1 text-muted" id="secondaryNews1Datetime">Nov 12</div>' +
     '<p class="card-text mb-auto" id="secondaryNews1Sample">This is a wider card with supporting text below as a natural lead-in to additional content.</p>' +
-    '<a href="#" class="stretched-link text-primary fw-semibold" id="secondaryNews1Link">Continue reading &#8811;</a>' +
+    '<button class="btn btn-sm btn-outline-primary fw-semibold" id="secondaryNews1Link">Continue reading &#8811;</button>' +
     "</div>" +
     '<div class="col-auto d-none d-lg-block">' +
     '<svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>' +
@@ -33,7 +33,7 @@ function getRecentNews() {
     '<h3 class="mb-0" id="secondaryNews2Title">Post title</h3>' +
     '<div class="mb-1 text-muted" id="secondaryNews2Datetime">Nov 11</div>' +
     '<p class="mb-auto" id="secondaryNews2Sample">This is a wider card with supporting text below as a natural lead-in to additional content.</p>' +
-    '<a href="#" class="stretched-link text-primary fw-semibold" id="secondaryNews2Link">Continue reading &#8811;</a>' +
+    '<button class="btn btn-sm btn-outline-primary fw-semibold" id="secondaryNews2Link">Continue reading &#8811;</button>' +
     "</div>" +
     '<div class="col-auto d-none d-lg-block">' +
     '<svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>' +
@@ -54,7 +54,7 @@ function getRecentNews() {
       const primaryLink = document.getElementById("primaryRecentNewsLink");
       primaryTitle.innerHTML = response[0].title;
       primarySample.innerHTML = getSampleText(response[0].full_article);
-      primaryLink.setAttribute("href", "/article/featured/0");
+      primaryLink.setAttribute("onclick", "viewNewsArticle(" + response[0].ID + ")");
 
       const secondary1Category = document.getElementById(
         "secondaryNews1Category"
@@ -70,7 +70,7 @@ function getRecentNews() {
       const date1 = new Date(response[1].post_datetime).toDateString();
       secondary1Datetime.innerHTML = date1;
       secondary1Sample.innerHTML = getSampleText(response[1].full_article);
-      secondary1Link.setAttribute("href", "/article/featured/1");
+      secondary1Link.setAttribute("onclick", "viewNewsArticle(" + response[1].ID + ")");
 
       const secondary2Category = document.getElementById(
         "secondaryNews2Category"
@@ -86,7 +86,7 @@ function getRecentNews() {
       const date2 = new Date(response[2].post_datetime).toDateString();
       secondary2Datetime.innerHTML = date2;
       secondary2Sample.innerHTML = getSampleText(response[2].full_article);
-      secondary2Link.setAttribute("href", "/article/featured/2");
+      secondary2Link.setAttribute("onclick", "viewNewsArticle(" + response[2].ID + ")");
     }
   };
 
@@ -116,4 +116,81 @@ function getCategory(num) {
     case 5:
       return "Random";
   }
+}
+
+function viewNewsArticle(articleID) {
+  const path = "/article/" + articleID;
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", path, true);
+
+  xhr.onload = function () {
+    if (this.status == 200) {
+      const response = JSON.parse(this.responseText);
+      const newsTitleLocation = document.getElementById("newsFocusedTitle");
+      const newsDatetimeLocation = document.getElementById("newsFocusedDate");
+      const newsTextLocation = document.getElementById("newsFocusedText");
+      const date = new Date(response[0].post_datetime).toDateString();
+
+      newsTitleLocation.innerHTML = response[0].title;
+      newsDatetimeLocation.innerHTML = date;
+      newsTextLocation.innerHTML = response[0].full_article;
+      grabArticleAuthor(response[0].user_id);
+    } else {
+      var alertPlaceholder = document.getElementById("tempAlert");
+      var temp = document.getElementById("alert-created");
+      var wrapper = document.createElement("div");
+      if (!temp) {
+        wrapper.innerHTML =
+          '<div id="alert-created" class="alert alert-' +
+          "danger" +
+          ' role="alert">' +
+          "There was a problem getting this news article!" +
+          "</div>";
+        alertPlaceholder.append(wrapper);
+      }
+      console.log(response.status);
+    }
+  };
+  xhr.send();
+}
+
+function grabArticleAuthor(userID) {
+  const path = "/get_user/" + userID;
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", path, true);
+
+  xhr.onload = function () {
+    if (this.status == 200) {
+      const response = JSON.parse(this.responseText);
+      const newsAuthorLocation = document.getElementById("newsFocusedAuthor");
+
+      newsAuthorLocation.innerHTML =
+        response[0].first_name + " " + response[0].last_name;
+      openNewsArticle();
+    } else {
+      var alertPlaceholder = document.getElementById("tempAlert");
+      var temp = document.getElementById("alert-created");
+      var wrapper = document.createElement("div");
+      if (!temp) {
+        wrapper.innerHTML =
+          '<div id="alert-created" class="alert alert-' +
+          "danger" +
+          ' role="alert">' +
+          "There was a problem getting the author of this news article!" +
+          "</div>";
+        alertPlaceholder.append(wrapper);
+      }
+      console.log(response.status);
+    }
+  };
+  xhr.send();
+}
+
+function openNewsArticle() {
+  console.log("Reached here!");
+  const modal = new bootstrap.Modal(
+    document.getElementById("newsFocusedModal")
+  );
+
+  modal.show();
 }
